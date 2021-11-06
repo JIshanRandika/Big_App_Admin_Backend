8cdec7084e66270cac7ab7cd6b04c626caaacc8c
JIshanRandika <jishanrandika@gmail.com>
2021-11-06 21:18:03 +0530

    item.router

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
+const Item = require('./app/models/item.model.js');
+
+
 const Role = db.role;
 
 // db.mongoose
@@ -44,9 +47,46 @@ const Role = db.role;
 const mongoose = require('mongoose');
 
 
-db.mongoose.connect(dbConfig.url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbConfig.url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(async () => {
       console.log("Successfully connected to MongoDB.");
+
+
+      // username: String,
+      //     itemID: String,
+      //     itemName: String,
+      //     quantity: String,
+      //     itemStatus: String,
+
+
+
+      const Items = [
+        { username: 'test', itemID: '1',
+          itemName: 'item1', quantity: 10,itemStatus:'Y'},
+        { username: 'test', itemID: '2',
+          itemName: 'item2', quantity: 15,itemStatus:'N'},
+        { username: 'test', itemID: '3',
+          itemName: 'item3', quantity: 20,itemStatus:'Y'},
+      ]
+
+      for(let i=0; i<Items.length; i++){
+
+        const item = new Item({
+          username: Items[i].username,
+          itemID: Items[i].itemID,
+          itemName: Items[i].itemName,
+          quantity: Items[i].quantity,
+          itemStatus: Items[i].itemStatus
+        });
+
+        // Save a Customer in the MongoDB
+        await item.save();
+      }
+
+
+
+
+
 
       Role.estimatedDocumentCount((err, count) => {
         if (!err && count === 0) {
@@ -111,19 +151,17 @@ db.mongoose.connect(dbConfig.url, { useNewUrlParser: true, useUnifiedTopology: t
 
 
 
-
-
-
-
-
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Ishan application." });
 });
 
 // routes
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
+require('./app/routes/item.router.js')(app);
+
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;

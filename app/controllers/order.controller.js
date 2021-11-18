74eac3c952a00006a5a90dea1599072e3aed6b02
JIshanRandika <jishanrandika@gmail.com>
2021-11-19 03:24:27 +0530

    createOrder controller

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -1,2 +1,25 @@
 const Order = require('../models/order.model.js');
 
+// POST a Order
+exports.createOrder = (req, res) => {
+
+
+
+    const order = new Order({
+        orderID: req.body.orderID,
+        shopName: req.body.shopName,
+        itemAndQuantity: req.body.itemAndQuantity,
+        acceptStatus: req.body.acceptStatus,
+        completeStatus: req.body.completeStatus,
+    });
+
+    // Save a Item in the MongoDB
+    order.save().then(data => {
+        res.status(200).json(data);
+    }).catch(err => {
+        res.status(500).json({
+            message: "Fail!",
+            error: err.message
+        });
+    });
+};

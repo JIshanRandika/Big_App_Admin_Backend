35bdf6f4d9cc297f0428ae5a659034536768f8ef
JIshanRandika <jishanrandika@gmail.com>
2021-11-19 03:10:41 +0530

    order model

---
+++ app/models/order.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+var name = 'order';
+
+const OrderSchema = mongoose.Schema({
+    orderID: String,
+    shopName: String,
+    itemAndQuantity: String,
+    acceptStatus: String,
+    completeStatus: String
+
+});
+
+module.exports = mongoose.model(`${name}`, OrderSchema);
